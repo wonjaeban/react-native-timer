4a840c610ba926b7457f5a57f006886ebe869248
반 원재 <wonjae0709@jinhakapply.com>
2023-06-28 18:40:32 +0900

    feat: 하나씩 올라가는 타이머 추가

---
+++ src/Timer.js
@@ -0,0 +1,52 @@
+import React, {useCallback, useEffect, useState, useRef} from 'react';
+import {Text, View} from 'react-native';
+
+const SelStuExamTimer = ({restartCondition}) => {
+  const [time, setTime] = useState(0);
+  const timer = useRef();
+
+  useEffect(() => {
+    restartTimer();
+  }, [restartCondition]);
+
+  const startTimer = () => {
+    const currentDate = new Date();
+    timer.current = setInterval(() => {
+      setTime(new Date() - currentDate);
+    }, 1000);
+    return () => clearInterval(timer.current);
+  };
+
+  const resetTimer = () => {
+    clearInterval(timer.current);
+    setTime(0);
+  };
+
+  const restartTimer = () => {
+    resetTimer();
+    startTimer();
+  };
+
+  const changeTimeToString = numberTime => {
+    if (numberTime < 10) {
+      return `0${numberTime}`;
+    }
+    return `${numberTime}`;
+  };
+
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: ds(22),
+          color: H_Colors.grey[6],
+          fontWeight: 'bold',
+        }}>
+        {changeTimeToString(Math.floor(time / 1000 / 60))}:
+        {changeTimeToString(Math.floor((time / 1000) % 60))}
+      </Text>
+    </View>
+  );
+};
+
+export default SelStuExamTimer;
